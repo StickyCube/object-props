fc113d6b639e87aa475b587e268fd16d689e075d
stickycube <will.jk01@gmail.com>
2015-10-29 00:21:17 +0000

    added tests to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,4 +2,18 @@
 
 module.exports = function (grunt) {
 
+  grunt.initConfig({
+    mocha_istanbul: {
+      test: {
+        src: 'tests/**/*.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-istanbul')
+
+  grunt.registerTask('build', []);
+  grunt.registerTask('test', [
+    'mocha_istanbul:test'
+  ]);
 };
